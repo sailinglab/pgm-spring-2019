7207f175ebb0fc43ccc8546e6c72c7cc591e3fb3
Ben <bstriner@gmail.com>
2019-02-23 01:35:33 -0500

    katex fixes

--- assets/js/katex.js
+++ assets/js/katex.js
@@ -1,8 +1,21 @@
+options = {
+	"macros":{
+		"\\abs":"\\left\\lvert #1 \\right\\rvert",
+		"\\parens":"\\left( #1 \\right)",
+		"\\squareb":"\\left[ #1 \\]",
+		"\\curlyb":"\\left\\{ #1 \\right\\}",
+		"\\cross":"\\left\\langle #1 \\right\\rangle",
+		"\\Eb":"\\mathbb{E}",
+		"\\Pb":"\\mathbb{P}",
+		"\\Rb":"\\mathbb{R}"
+	}
+};
+
 $("script[type='math/tex']").replaceWith(
   function(){
     var tex = $(this).text();
     return "<span class=\"inline-equation\">" +
-           katex.renderToString(tex) +
+           katex.renderToString(tex, options) +
            "</span>";
 });
 
@@ -10,6 +23,6 @@ $("script[type='math/tex; mode=display']").replaceWith(
   function(){
     var tex = $(this).text();
     return "<div class=\"equation\">" +
-           katex.renderToString("\\displaystyle "+tex) +
+           katex.renderToString("\\displaystyle "+tex, options) +
            "</div>";
 });
